dbecd627095a191ef3b3f88e13a67be2ce3e9d4a
dinitrogen <noshowdon@gmail.com>
2022-06-20 18:35:12 -0400

    added simple BSO counter

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.scorekeeper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.scorekeeper.ui.theme.ScorekeeperTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ButtonBar()
                 }
             }
         }
@@ -30,14 +33,97 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ButtonBar() {
+
+    var numBalls by remember { mutableStateOf(0) }
+    var numStrikes by remember { mutableStateOf(0) }
+    var numOuts by remember { mutableStateOf(0) }
+
+    fun addBall() {
+        if (numBalls > 2) {
+            numBalls = 0
+            numStrikes = 0
+        } else {
+            numBalls++
+        }
+    }
+
+    fun addStrike(func:()-> Unit) {
+        if (numStrikes > 1) {
+            numStrikes = 0
+            numBalls = 0
+            func()
+        } else {
+            numStrikes++
+        }
+    }
+
+    fun addOut() {
+        if (numOuts > 1) {
+            numOuts = 0
+            numStrikes = 0
+            numBalls = 0
+        } else {
+            numOuts++
+            numStrikes = 0
+            numBalls = 0
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            //horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Button(
+                modifier = Modifier.padding(6.dp),
+                onClick = { addBall() })
+            {
+                Text(text = "ball")
+            }
+
+            Button(
+                modifier = Modifier.padding(6.dp),
+                onClick = { addStrike({ addOut() }) }) {
+                Text(text = "strike")
+            }
+
+            Button(
+                modifier = Modifier.padding(6.dp),
+                onClick = { addOut() }) {
+                Text(text = "out")
+            }
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(
+                text = "Balls: $numBalls",
+                modifier = Modifier.padding(6.dp)
+            )
+
+            Text(
+                text = "Strikes: $numStrikes",
+                modifier = Modifier.padding(6.dp)
+            )
+
+            Text(
+                text = "Outs: $numOuts",
+                modifier = Modifier.padding(6.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ScorekeeperTheme {
-        Greeting("Android")
+        ButtonBar()
     }
 }
